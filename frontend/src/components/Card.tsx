a311ad6cd19d81a7e60768efb6875c9955f9e577
Jin Hong Pang <jinhong36@icloud.com>
2026-04-19 14:59:03 +1000

    Update Card component styles to increase text size on desktop

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -34,18 +34,18 @@ export default function Card({ img, name, role, bio, isHidden, onOpen, onClose }
                 <img className="w-full aspect-[2/3] object-cover" src={img} alt="profile-picture" />
             </div>
             <div className="mt-3 xl:flex xl:justify-between">
-                <p className="font-cormorant font-bold text-sm md:text-lg text-wood-dark">{name}</p>
-                <p className="font-cormorant text-sm md:text-lg text-wood-accent xl:self-center">{role}</p>
+                <p className="font-cormorant font-bold text-sm md:text-md text-wood-dark">{name}</p>
+                <p className="font-cormorant text-sm md:text-md text-wood-accent xl:self-center">{role}</p>
             </div>
 
             {isOpen && createPortal(
                 <div className={`fixed inset-5 z-60 p-10 flex items-start pt-24 justify-center md:items-center md:pt-10 ${isClosing ? 'animate-fade-out' : 'animate-fade-in'}`} onClick={handleClose}>
                     <div className="flex flex-col items-center md:flex-row md:items-center gap-4 md:gap-10 max-w-4xl w-full">
-                        <img className="card w-[40vw] aspect-2/3 md:w-60 object-cover shrink-0" src={img} alt="profile-picture" />
+                        <img className="card w-[45vw] aspect-2/3 md:w-60 object-cover shrink-0" src={img} alt="profile-picture" />
                         <div className="flex flex-col text-center md:text-left">
-                            <p className="font-cormorant text-md md:text-lg text-wood-dark font-bold">{name}</p>
-                            <p className="font-cormorant text-md md:text-lg text-wood-dark font-bold">{role}</p>
-                            <p className="font-cormorant pt-6 text-sm md:text-md text-wood-accent">{bio}</p>
+                            <p className="font-cormorant text-md md:text-xl text-wood-dark font-bold">{name}</p>
+                            <p className="font-cormorant text-md md:text-xl text-wood-dark font-bold">{role}</p>
+                            <p className="font-cormorant pt-6 text-sm md:text-lg text-wood-accent">{bio}</p>
                         </div>
                     </div>
                 </div>,
