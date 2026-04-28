5254281eb8090097f217eec05b66d1ae9aaf7083
Jin Hong Pang <jinhong36@icloud.com>
2026-04-28 19:12:52 +1000

    style: update About page layout for improved alignment and text responsiveness

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -12,17 +12,16 @@ export default function About() {
 
   return (
     <>
-    <div className="flex-1 flex items-center justify-center">
-      <div className="w-[70vw] md:w-[45vw] lg:w-[30vw] flex flex-col items-center gap-8">
-                      
+    <div className="flex-1 flex items-start justify-center">
+      <div className="w-[70vw] md:w-[45vw] lg:w-[30vw] flex flex-col items-center gap-8 mt-[40vh] md:mt-[25vh]">            
         <GrandOpening />
 
-        <p className="opacity-0 animate-text-in [animation-delay:0ms] text-[clamp(0.9rem,1.3vw,1.5rem)] text-center font-playfair text-wood-dark/80">
+        <p className="opacity-0 animate-text-in [animation-delay:0ms] text-[clamp(0.9rem,1.6vw,1.2rem)] text-center font-playfair text-wood-dark/80">
           IC Method is a Barre × Pilates atelier born from a lifelong devotion to movement. Here, dance heritage meets precise, aesthetic instruction. Every session unfolds through line, rhythm, and breath—guiding bodies toward control, expression, and mindful connection. It is a quiet, considered space to return to yourself. Hawthorn.
         </p>
         <Link
           to="/booking"
-          className=" opacity-0 animate-text-in [animation-delay:200ms] inline-flex items-center gap-3 font-didot text-xs tracking-[0.2em] uppercase bg-wood-accent/90 text-wood-text hover:bg-wood-dark transition-colors duration-300 py-3 px-8 rounded-md group"
+          className=" opacity-0 animate-text-in [animation-delay:200ms] inline-flex items-center gap-3 font-didot text-xs tracking-[0.2em] uppercase bg-wood-accent/90 text-wood-text text-[clamp(0.7rem,1.5vw,0.85rem)] hover:bg-wood-dark transition-colors duration-300 py-3 px-8 rounded-md group"
         >
           <>
             <span className="hidden md:flex">Book Now</span>
